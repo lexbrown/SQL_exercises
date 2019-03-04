6ba489b4281c5e75754b223d50b79fd48af6c7c9
LexBrown <44871845+lexbrown@users.noreply.github.com>
2019-03-04 16:21:49 +0300

    Update test new hints.sql

--- test new hints.sql
+++ test new hints.sql
@@ -1,4 +1,4 @@
-SELECT idcontractstatus, AVG(idcontract)  AS CNT , CASE WHEN idcontractstatus < 15 THEN 'Дела писос'
-ELSE 'Дела заебос' END GHJ
+SELECT idcontractstatus, AVG(idcontract)  AS CNT , CASE WHEN idcontractstatus < 15 THEN 'Дела foo'
+ELSE 'Дела bar' END GHJ
 FROM Contract.Contract
 GROUP BY idcontractstatus WITH ROLLUP
